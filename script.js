027782ecffb1f6abf400db1e719d50955352f3af
Lina-Sim <simsion38@gmail.com>
2026-07-10 11:55:33 +0900

    Update homepage

--- script.js
+++ script.js
@@ -1,90 +1,39 @@
+window.addEventListener("DOMContentLoaded", () => {
 
-const pages = [
+    const pageFlip = new St.PageFlip(
+        document.getElementById("book"),
+        {
+            width:700,
+            height:990,
 
-"images/cover-vol01.png",
+            size:"stretch",
 
-"pages/vol01-page02.jpg",
+            minWidth:315,
+            maxWidth:700,
 
-"pages/vol01-page03.jpg",
+            minHeight:450,
+            maxHeight:990,
 
-"pages/vol01-page04.jpg",
+            showCover:true,
 
-"pages/vol01-page05.jpg",
+            mobileScrollSupport:true
+        }
+    );
 
-"pages/vol01-page06.jpg",
+    pageFlip.loadFromImages([
+        "images/cover-vol01.png",
 
-"pages/vol01-page07.jpg",
-
-"pages/vol01-page08.jpg",
-
-"pages/vol01-page09.jpg",
-
-"pages/vol01-page10.jpg",
-
-"pages/vol01-page11.jpg",
-
-"pages/vol01-page12.jpg"
-
-];
-
-let current = 0;
-
-const img = document.getElementById("pageImage");
-
-const page = document.getElementById("currentPage");
-
-document.getElementById("nextBtn").onclick = ()=>{
-
-    if(current < pages.length-1){
-
-        current++;
-
-        updatePage();
-
-    }
-
-};
-
-document.getElementById("prevBtn").onclick = ()=>{
-
-    if(current>0){
-
-        current--;
-
-        updatePage();
-
-    }
-
-};
-
-function updatePage(){
-
-    img.style.opacity=0;
-
-    setTimeout(()=>{
-
-        img.src=pages[current];
-
-        page.textContent=current+1;
-
-        img.style.opacity=1;
-
-    },180);
-
-}
-
-document.addEventListener("keydown",(e)=>{
-
-    if(e.key==="ArrowRight"){
-
-        document.getElementById("nextBtn").click();
-
-    }
-
-    if(e.key==="ArrowLeft"){
-
-        document.getElementById("prevBtn").click();
-
-    }
+        "pages/vol01-page02.jpg",
+        "pages/vol01-page03.jpg",
+        "pages/vol01-page04.jpg",
+        "pages/vol01-page05.jpg",
+        "pages/vol01-page06.jpg",
+        "pages/vol01-page07.jpg",
+        "pages/vol01-page08.jpg",
+        "pages/vol01-page09.jpg",
+        "pages/vol01-page10.jpg",
+        "pages/vol01-page11.jpg",
+        "pages/vol01-page12.jpg"
+    ]);
 
 });
