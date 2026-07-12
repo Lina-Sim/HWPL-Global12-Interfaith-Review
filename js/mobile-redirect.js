e1dd76fa5bfb2fe6da00262d2277cea9f71f5ca7
Lina-Sim <simsion38@gmail.com>
2026-07-12 17:59:19 +0900

    Update mobile-redirect.js

--- js/mobile-redirect.js
+++ js/mobile-redirect.js
@@ -1,9 +1,7 @@
 const isMobile = window.matchMedia("(max-width: 768px)").matches;
 
 if (isMobile) {
-
     setTimeout(() => {
         window.location.replace("mobile-viewer.html");
     }, 100);
-
 }
