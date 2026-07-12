30612684f65c34013c153a79910e29073439f4f4
Lina-Sim <simsion38@gmail.com>
2026-07-12 17:36:55 +0900

    Update mobilde-redirect.js

--- js/mobilde-redirect.js
+++ js/mobilde-redirect.js
@@ -1,6 +1,6 @@
-window.addEventListener("DOMContentLoaded", () => {
+const isMobile = window.matchMedia("(max-width: 768px)").matches;
 
+if (isMobile) {
     window.location.href =
     "pdf-viewer/viewer.html?file=../issues/vol01.pdf";
-
-});
+}
