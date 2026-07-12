439783ed81726f4a6026a8d9c1aee1c6d16224c5
Lina-Sim <simsion38@gmail.com>
2026-07-12 17:56:38 +0900

    Update mobile-redirect.js

--- js/mobile-redirect.js
+++ js/mobile-redirect.js
@@ -1,7 +1,9 @@
-alert("redirect loaded");
-
 const isMobile = window.matchMedia("(max-width: 768px)").matches;
 
 if (isMobile) {
-    window.location.replace("mobile-viewer.html");
+
+    setTimeout(() => {
+        window.location.replace("mobile-viewer.html");
+    }, 100);
+
 }
